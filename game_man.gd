013d8c88e84238201c085faeb441350f1d203ece
Norrick McGee <Norrick.McGee@gmail.com>
2024-05-22 21:07:16 -0500

    removed bloated actor_man.gd script from MultiplayerSpawner moved to GameMan

--- game_man.gd
+++ game_man.gd
@@ -11,13 +11,26 @@ var spawn_points = [
 ]
 
 func _ready():
+	ActorMan.spawn_function = custom_spawn
 	add_player(1)
 
 func add_player(id):
+	"""
+	add_player is called by game_man._ready() and by main_man.host_add_player(id)
+	"""
 	if not multiplayer.is_server(): return
 	var spawn_position = spawn_points[players.size()]
-	players[id] = 0 # Using players(Dictionary) as a set datatype, so fill it with empty data 0
-	ActorMan.add_player(id, spawn_position)
+	ActorMan.spawn([id, spawn_position])
+
+func custom_spawn(vars):
+	var id = vars[0]
+	var pos = vars[1]
+	var p = Actor.instantiate()
+	players[id] = p
+	p.MultiplayerSyncInput.set_multiplayer_authority(id)
+	p.name = str(id) 
+	p.position = pos
+	return p
 
 func remove_player(id):
 	pass
