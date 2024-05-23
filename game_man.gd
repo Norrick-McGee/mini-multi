6b49500a05a66efd2177c977b33b5b247440f79d
Norrick McGee <Norrick.McGee@gmail.com>
2024-05-22 21:22:46 -0500

    minor code cleanup and documentation adds

--- game_man.gd
+++ game_man.gd
@@ -17,14 +17,31 @@ func _ready():
 func add_player(id):
 	"""
 	add_player is called by game_man._ready() and by main_man.host_add_player(id)
+	
+	args
+	---------
+	id (int): 
+	   player_id according to mutliplayer
 	"""
 	if not multiplayer.is_server(): return
 	var spawn_position = spawn_points[players.size()]
 	ActorMan.spawn([id, spawn_position])
 
-func custom_spawn(vars):
-	var id = vars[0]
-	var pos = vars[1]
+func custom_spawn(args: Array) -> CharacterBody2D:
+	"""
+	args
+	---------
+	id (int): 
+	   player_id according to mutliplayer
+	pos (Vector2):
+		position to spawn, probably picked from GameMan.spawn_points (Array[Vector2])
+	returns
+	---------
+	p (CharacterBody2D):
+		the player to spawn. I believe this needs to be returned in order to spawn it
+	"""
+	var id = args[0]
+	var pos = args[1]
 	var p = Actor.instantiate()
 	players[id] = p
 	p.MultiplayerSyncInput.set_multiplayer_authority(id)
--- main_man.gd
+++ main_man.gd
@@ -8,17 +8,12 @@ func start_game(kwargs: Dictionary):
 	if multiplayer.is_server():
 		multiplayer.peer_connected.connect(host_add_player)
 		multiplayer.peer_disconnected.connect(host_drop_player)
-		# host_add_player(0) # TODO: Do we need to spawn the host lol?
 	else:
 		pass
-		
-
+	
 func host_add_player(id):
-	# TODO: get player contents from player
 	GameMan.add_player(id)
 
-
 func host_drop_player(id):
-	print("Player Disconnected: {id}".format({"id":id}))
 	GameMan.remove_player(id)
 	
